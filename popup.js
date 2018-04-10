176ab46dde74f832a7bae646fcf7e883c0eebb69
meant4ekansh <37782224+meant4ekansh@users.noreply.github.com>
2018-04-10 21:08:40 +0530

    counting numbers/word through Javascript

---
+++ popup.js
@@ -0,0 +1,23 @@
+var countTextCalled = 0;
+function countText(a){
+    countTextCalled++;
+    var dig = 0;
+    var  letters = 0;
+    for(var i = 0; i < a.length; i++){
+        if(isNaN(a[i]))
+        letters +=1;
+        else
+        dig +=1;
+    }
+var spaces = a.split(" ").length - 1;
+
+    document.getElementById('resultsLetter').innerHTML = letters;
+    document.getElementById('resultsDigits').innerHTML = dig - spaces;
+    document.getElementById('resultsSpaces').innerHTML = spaces;
+    document.getElementById('resultscounting').innerHTML = countTextCalled;
+    document.getElementById('colours').style.backgroundColor = "Blue";
+    var inputBox = document.createElement('input');
+    inputBox.type = "radio";
+    document.body.appendChild(inputBox);
+
+}
